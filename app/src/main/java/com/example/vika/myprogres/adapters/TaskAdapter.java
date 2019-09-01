1e6b58efb2af8efafb147936092d9cf999244d7f
anarion-Vika <anarion.vika@gmail.com>
2019-09-01 11:00:14 +0300

    Task adapter

--- app/src/main/java/com/example/vika/myprogres/adapters/TaskAdapter.java
+++ app/src/main/java/com/example/vika/myprogres/adapters/TaskAdapter.java
@@ -1,4 +1,56 @@
 package com.example.vika.myprogres.adapters;
 
-public class TaskAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vika.myprogres.R;
+import com.example.vika.myprogres.models.TaskModel;
+
+import java.util.ArrayList;
+
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+    Context context;
+    ArrayList<TaskModel> arrayList = new ArrayList<>();
+
+    public TaskAdapter(Context context, ArrayList<TaskModel> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+
+    @NonNull
+    @Override
+    public TaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.model_task, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(arrayList.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(TaskModel model){
+
+        }
+    }
+
 }
